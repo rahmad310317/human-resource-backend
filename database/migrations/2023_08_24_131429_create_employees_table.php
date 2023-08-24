758a3f3ec7e0cc14f8e4749197e2549e4ab09b21
rahmad310317 <rahmadwindisimanullang@gmail.com>
2023-08-24 21:24:05 +0700

    update model

---
+++ database/migrations/2023_08_24_131429_create_employees_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use PhpParser\Node\NullableType;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('employees', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->string('gender')->nullable();
+            $table->integer('phone')->nullable();
+            $table->string('photo')->nullable();
+            $table->bigInteger('team_id')->unsigned();
+            $table->bigInteger('role_id')->unsigned();
+
+
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('employees');
+    }
+};
