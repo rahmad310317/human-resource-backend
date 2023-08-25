701dadb431d0daf4ea3956ada7a8759642c98c25
rahmad310317 <rahmadwindisimanullang@gmail.com>
2023-08-25 20:22:49 +0700

    setup laravel jetstream dan blade

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,13 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified',
+])->group(function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+});
