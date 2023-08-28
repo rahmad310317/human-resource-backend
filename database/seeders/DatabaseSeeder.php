8e7d6b3fe489947d9a75fd987bc02ee64904a15f
rahmad310317 <rahmadwindisimanullang@gmail.com>
2023-08-28 13:16:33 +0700

    update factori model

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,11 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\Role;
+use App\Models\Team;
+use App\Models\User;
+use App\Models\Company;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -12,11 +17,16 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+        // User::factory(2)->unverified()->create();
+        // Company::factory(10)->create();
+        // Team::factory(30)->create();
+        Role::factory(30)->create();
 
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+
+
     }
 }
