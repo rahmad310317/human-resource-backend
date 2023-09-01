3387c41af32f1f4c262e4d16ee31364167a6b8a7
rahmad310317 <rahmadwindisimanullang@gmail.com>
2023-09-01 20:13:37 +0700

    update companycontroller

---
+++ app/Http/Controllers/API/CompanyController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class CompanyController extends Controller
+{
+    public function all()
+    {
+    }
+}
