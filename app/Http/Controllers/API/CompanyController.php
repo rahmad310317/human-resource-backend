5223509377d8c0a4fc3e649ad724932fca1aa443
rahmad310317 <rahmadwindisimanullang@gmail.com>
2023-09-25 12:30:29 +0700

    update isset logo

--- app/Http/Controllers/API/CompanyController.php
+++ app/Http/Controllers/API/CompanyController.php
@@ -96,7 +96,7 @@ class CompanyController extends Controller
             // Update company
             $company->update([
                 'name' => $request->name,
-                'logo' => $path
+                'logo' => isset($path) ? $path : $company->icon,
             ]);
 
             return ResponseFormatter::success($company, 'Company updated');
